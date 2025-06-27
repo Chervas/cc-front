0c5c5b7429eb88840962a4ab57a285e5ef738133
Carlos Hervas <carlos.hervas@modmarketing.net>
2025-06-27 13:42:49 +0200

    Eliminar carpeta backendclinicaclick del repo cc-front

--- backend_clinicaclick/src/test.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const password = "123456hH";
-const hash = "$2a$08$vt8tIfRmZrzJnwTwClWrY.KYK78dB89TKAuCrVoY0xjVi17rVQ1xO";
-
-bcrypt.compare(password, hash)
-  .then(result => console.log('Resultado:', result))
-  .catch(err => console.error('Error:', err));
